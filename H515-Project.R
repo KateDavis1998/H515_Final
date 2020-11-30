ac5da7618c74a44cf32b254e12fd2efa3e68ca2c
iexiak <69724759+iexiak@users.noreply.github.com>
2020-11-30 00:53:04 -0500

    Update H515-Project.R

--- H515-Project.R
+++ H515-Project.R
@@ -5,6 +5,12 @@ library(gridExtra)
 library(dplyr)
 #install.packages("openxlsx")
 library(openxlsx)
+library(caret)
+library(ROCR)
+library(glmnet)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
 
 #Set WD
 #setwd('/Users/paigescott/Documents/IUPUI/INFO-H515/Project') #Paige
@@ -111,15 +117,13 @@ scans.min <- scans[,-c(1,2,3,5,13)]
 head(scans.min)
 
 #splitting into test/train
-library(caret)
-library(ROCR)
 set.seed(1234)
 split = createDataPartition(scans.min$cf_stdbin, p = 0.7, list = FALSE)
 scans.min.train = scans.min[split,]
 scans.min.test = scans.min[-split,]
 
 #some basic regression
-#note had to remove examcode as it creates too many factors..
+#note had to remove examcode/radiologist as it creates too many factors..
 logscans = glm(cf_stdbin ~ shift+hr_cmpl+hr_dict+dow_cmpl+resdict+modality+priority+orgcode+eio+sect, data=scans.min.train, family="binomial")
 summary(logscans)
 pred = predict(logscans, newdata=scans.min.test, type="response")
@@ -133,3 +137,115 @@ truePos
 falsePos = confusion.matrix[1,2]/sum(confusion.matrix[1,])
 falsePos
 
+
+#Lasso/Ridge setup
+#note - these take a long time to run, and have negative osr^2 
+x.train=model.matrix(cf_stdbin ~ shift+hr_cmpl+hr_dict+dow_cmpl+resdict+examcode+modality+priority+orgcode+eio+sect+radiologist,data=scans.min.train)
+#x.train=model.matrix(cf_stdbin ~ shift+hr_cmpl+hr_dict+dow_cmpl+resdict+modality+priority+orgcode+eio+sect,data=scans.min.train)
+y.train=scans.min.train$cf_stdbin
+x.test=model.matrix(cf_stdbin ~ shift+hr_cmpl+hr_dict+dow_cmpl+resdict+examcode+modality+priority+orgcode+eio+sect+radiologist,data=scans.min.test) 
+#x.test=model.matrix(cf_stdbin ~ shift+hr_cmpl+hr_dict+dow_cmpl+resdict+modality+priority+orgcode+eio+sect,data=scans.min.test) 
+y.test=scans.min.test$cf_stdbin
+all.lambdas <- c(exp(seq(15, -10, -.1)))
+
+#Ridge
+ridge.cv=cv.glmnet(x = x.train, y = y.train, alpha=0, lambda=all.lambdas, family = "binomial")
+plot(ridge.cv)
+best.lambda.ridge <- ridge.cv$lambda.min
+ridge.mse.min <- ridge.cv$cvm[ridge.cv$lambda == ridge.cv$lambda.min]
+ridge.mse.min
+
+ridge.tr=glmnet(x = x.train, y = y.train, alpha=0, lambda=best.lambda.ridge, family = "binomial")
+ridge.tr$beta # all nonzero
+ridge.r2 = ridge.tr$dev.ratio
+ridge.r2
+ridge.pred = predict(ridge.tr, newx=x.test, type="response")
+SSE<-sum((ridge.pred-as.numeric(y.test))^2)
+SST<-sum((mean(as.numeric(y.train))-as.numeric(y.test))^2)
+ridge.OSR2<-1-SSE/SST
+ridge.OSR2 #This is negative, so it fits worse than a horizontal line :/
+ridge.OSMSE<-SSE/nrow(scans.min.test)
+ridge.OSMSE
+
+#Lasso
+lasso.cv=cv.glmnet(x = x.train, y = y.train, alpha=1, lambda=all.lambdas, family = "binomial")
+plot(lasso.cv)
+best.lambda.lasso <- lasso.cv$lambda.min
+lasso.mse.min <- lasso.cv$cvm[lasso.cv$lambda == lasso.cv$lambda.min]
+lasso.mse.min
+
+# This is how I did the assignment, but throws errors, which is indicative that this is the wrong thing to do
+# https://stackoverflow.com/questions/32184776/glmnet-not-converging-for-lambda-min-from-cv-glmnet
+# lasso.tr=glmnet(x.train,y.train, alpha=1, lambda=best.lambda.lasso, exact=FALSE, family = "binomial")
+# lasso.tr$beta
+# lasso.r2 = lasso.tr$dev.ratio
+# lasso.r2
+
+lasso.pred = predict(lasso.cv, newx=x.test, s = "lambda.min", type="response")
+SSE<-sum((lasso.pred-as.numeric(y.test))^2)
+SST<-sum((mean(as.numeric(y.train))-as.numeric(y.test))^2)
+lasso.OSR2<-1-SSE/SST
+lasso.OSR2 #This is negative, so it fits worse than a horizontal line :/
+lasso.OSMSE<-SSE/nrow(scans.min.test)
+lasso.OSMSE
+
+
+
+#CART/Random Forest setup
+#also negative :/
+x.train = scans.min.train[,-c(1,2,16,17)]
+x.test = scans.min.test[,-c(1,2,16,17)]
+y.train = scans.min.train[,ncol(scans.min.train)]
+y.test = scans.min.test[,ncol(scans.min.train)]
+
+#CART
+train.cart = train(x.train,y.train,method="rpart",
+                   tuneGrid=data.frame(cp=seq(0, 0.02, 0.001)),trControl=trainControl(method="cv", number=10), minbucket=5)
+train.cart$results[,"RMSE"]
+train.cart$bestTune
+mod.cart = train.cart$finalModel
+pred.cart = predict(mod.cart, newdata=scans.min.test)
+r2.cart = 1-sum((as.numeric(y.test) - pred.cart)^2)/sum((as.numeric(y.test) - mean(as.numeric(y.train)))^2)
+r2.cart
+error.cart = as.numeric(y.test) - pred.cart
+sqrt(mean(error.cart^2)) #OSRMSE
+sqrt(mean(abs(error.cart))) #OSMAE
+
+prp(mod.cart, varlen=0)
+mod.cart
+
+train.cart$results
+
+
+#Random Forest
+#removing variables with more than 53 categories..
+x.train = x.train[,-c(7,13)]
+x.test = x.test[,-c(7,13)]
+rf = randomForest(cf_stdbin~shift+hr_cmpl+hr_dict+dow_cmpl+resdict+modality+priority+orgcode+eio+sect, data=scans.min.train, mtry = 3, nodesize=25, ntree = 80)
+train.rf.oob = train(x.train, y.train, method="rf", tuneGrid=data.frame(mtry=1:10),
+                     trControl=trainControl(method="oob"))
+
+train.rf.oob$results
+
+plot(train.rf.oob$results$mtry,train.rf.oob$results$Rsquared,
+     xlab = "mtry",ylab = "Out-of-Bag R^2",type = "l")
+#wow this plot is bad
+
+#the train function stores the resulting final model
+#this doesn't work, not sure why
+# mod.rf = train.rf.oob$finalModel
+# mod.rf$mse
+# 
+# sqrt(tail(mod.rf$mse, 1)) #In Sample RMSE
+# r2.is.rf = 1-sum((y.train - mod.rf$predicted)^2)/sum((y.train - mean(y.train))^2) #in sample R2?
+# r2.is.rf
+# error.is.rf = y.train - mod.rf$predicted
+# sqrt(mean(error.is.rf^2)) #ISRMSE double check
+# sqrt(mean(abs(error.is.rf))) #ISMAE
+# 
+# pred.rf = predict(mod.rf, newdata=ames.test)
+# r2.rf = 1-sum((y.test - pred.rf)^2)/sum((y.test - mean(y.train))^2)
+# r2.rf
+# error.rf = y.test - pred.rf
+# sqrt(mean(error.rf^2)) #OSRMSE
+# sqrt(mean(abs(error.rf))) #OSMAE
